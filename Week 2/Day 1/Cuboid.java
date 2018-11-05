71134cd89123ffc65a13d4328d67fa56deb21da5
Chika <ikechikaodili@gmail.com>
2018-11-05 23:49:41 +0100

    added week 2 files

---
+++ Week 2/Day 1/Cuboid.java
@@ -0,0 +1,22 @@
+public class Cuboid {
+    public static void main(String[] args) {
+
+        // Write a program that stores 3 sides of a cuboid as variables (doubles)
+        // The program should write the surface area and volume of the cuboid like:
+        //
+        // Surface Area: 600
+        // Volume: 1000
+
+        double cuboidSideA = 10.2;
+        double cuboidSideB = 15.882;
+        double cuboidSideC = 20.881;
+
+        double surfaceArea = 2 * (cuboidSideA * cuboidSideB + cuboidSideB * cuboidSideC + cuboidSideA * cuboidSideC);
+        double volume = cuboidSideA * cuboidSideB * cuboidSideC;
+
+        System.out.println("Surface Area: " + surfaceArea);
+        System.out.println("Volume: " + volume);
+
+    }
+
+}
---
+++ Week 2/Day 2/Third.java
@@ -0,0 +1,14 @@
+public class Ex07Third {
+  public static void main(String[] args) {
+
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+    int[] q = {4, 5, 6, 7};
+    chooseNum(q);
+
+  }
+  public static void chooseNum (int[] q){
+    System.out.println(q[2]);
+    }
+}

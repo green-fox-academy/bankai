245bb2a1fc84c15a96d3694a8880e7981c144629
Chika <ikechikaodili@gmail.com>
2018-11-06 18:56:42 +0100

    added day 2 files

--- Week Three/src/TakesLonger.java
+++ Week Three/src/TakesLonger.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class TakesLonger {
+    public static void main(String... args) {
+        String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+        // When saving this quote a disk error has occurred. Please fix it.
+        // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+        // Using pieces of the quote variable (instead of just redefining the string)
+
+        quote = quote.replace("It you", "It always takes longer than you");
+
+        //String[] parts = quote.split("It", 2);
+        //quote = parts[0] + "It always takes longer than" + parts[1];
+        System.out.println(quote);
+    }
 }
--- Week Three/src/ToDoPrint.java
+++ Week Three/src/ToDoPrint.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class ToDoPrint {
+    public static void main(String... args) {
+        String todoText = " - Buy milk\n";
+        // Add "My todo:" to the beginning of the todoText
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected outpt:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+
+        todoText = "My todo:\n" + todoText + " - Download games\n" + "   - Diablo\n";
+        System.out.println(todoText);
+    }
 }

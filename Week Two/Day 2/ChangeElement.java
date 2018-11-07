d3ef4e290e93f2dd7f68b61746cf68c85ad888d0
Chika <ikechikaodili@gmail.com>
2018-11-07 10:33:56 +0100

    removed duplicate folder

--- Week Two/Day 2/ChangeElement.java
+++
@@ -1,7 +0,0 @@
-public class Ex10ChangeElement {
-  public static void main(String[] args) {
-    int[] s ={1, 2, 3, 8, 5, 6};
-    s[3] = 4;
-    System.out.println(s[3]);
-  }
-}
--- Week Two/Day 2/IncrementElement.java
+++
@@ -1,7 +0,0 @@
-public class Ex11IncrementElement {
-  public static void main(String[] args) {
-    int[] t = {1, 2, 3, 4, 5};
-    t[2]++;
-    System.out.println(t[2]);
-  }
-}

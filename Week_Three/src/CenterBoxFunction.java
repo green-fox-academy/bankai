91589ea8ae562763810a7695ec1c77a4798ebb1a
Chika <ikechikaodili@gmail.com>
2018-11-07 17:49:21 +0100

    added changes

--- Week_Three/src/CenterBoxFunction.java
+++ Week_Three/src/CenterBoxFunction.java
@@ -30,8 +30,8 @@ public class CenterBoxFunction {
 
     public static void centeredSquareDrawer(int squareSize, Graphics graphics) {
         graphics.setColor(new Color(0, 255, 0, 50));
-        // graphics.fillRect((WIDTH / 2 - (squareSize / 2)), (HEIGHT / 2 - (squareSize / 2)), squareSize, squareSize);
-        graphics.fillRect(0,0,WIDTH / 2 , HEIGHT / 2);
+        graphics.fillRect((WIDTH / 2 - (squareSize / 2)), (HEIGHT / 2 - (squareSize / 2)), squareSize, squareSize);
+        //graphics.fillRect((WIDTH /2 - squareSize/2), HEIGHT /,WIDTH / 2 , HEIGHT / 2);
     }
 
     public static void main(String[] args) {
@@ -41,6 +41,7 @@ public class CenterBoxFunction {
         jFrame.add(new ImagePanel());
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
+        //jFrame.pack();
     }
 
     static class ImagePanel extends JPanel {

de70ec2655cef92362bd57335a0a8d1d5637431d
Chika <ikechikaodili@gmail.com>
2018-11-15 17:10:44 +0100

    added new exercises

--- Week_Four/Day_four/src/Bunnies_2.java
+++ Week_Four/Day_four/src/Bunnies_2.java
@@ -4,10 +4,22 @@ public class Bunnies_2 {
     public static void main(String[] args) {
         System.out.println("How many rabbits dost thou desire?");
         Scanner blueEars = new Scanner(System.in);
-        System.out.println();
+        int rabbitEars = blueEars.nextInt();
+        System.out.println(bunnyEarNumber(rabbitEars));
     }
 
     private static int bunnyEarNumber(int bunnyTotal) {
-        int bunnyEars
+        int bunnyEars = 2;
+        if (bunnyTotal == 1) {
+            return bunnyEars;
+        }  else {
+            if (bunnyTotal % 2 == 0) {
+                return bunnyEars + 1 + bunnyEarNumber(bunnyTotal - 1);
+            }  else {
+                return bunnyEars + bunnyEarNumber(bunnyTotal - 1);
+            }
+        }
     }
 }
+
+

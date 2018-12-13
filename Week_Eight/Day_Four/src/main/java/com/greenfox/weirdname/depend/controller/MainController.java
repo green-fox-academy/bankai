1022104cce4d99024dd05533b62b22134683adeb
Chika <ikechikaodili@gmail.com>
2018-12-13 17:44:41 +0100

    added MainController

---
+++ Week_Eight/Day_Four/src/main/java/com/greenfox/weirdname/depend/controller/MainController.java
@@ -0,0 +1,87 @@
+package com.greenfox.weirdname.depend.controller;
+
+import com.greenfox.weirdname.depend.Service.StudentServiceInt;
+import com.greenfox.weirdname.depend.Service.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+  @Autowired
+  UtilityService utilityService;
+
+  @Autowired
+  StudentServiceInt studentService;
+
+  @GetMapping("/useful")
+  public String showUsefulPage() {
+    return "useful";
+  }
+
+  @GetMapping("/useful/colored")
+  public String showColoredBackgroundPage(Model model) {
+    model.addAttribute("utilityservice", utilityService.randomColor());
+    return "colored";
+  }
+
+  @GetMapping("/useful/email")
+  public String showEmailValidationPage(@RequestParam ("email") String email, Model model) {
+    model.addAttribute("validator", utilityService.validateEmail(email));
+    model.addAttribute("email", email);
+    return "email";
+  }
+
+  @GetMapping("/useful/encode")
+  public String encodingPage(@RequestParam ("text") String text, @RequestParam ("number") int number, Model model) {
+    model.addAttribute("codedtext", utilityService.pentatonic(text, number));
+    return "coding";
+  }
+
+  @GetMapping("/useful/decode")
+  public String decodingPage(@RequestParam ("text") String text, @RequestParam ("number") int number, Model model) {
+    model.addAttribute("codedtext", utilityService.pentatonic(text, -number));
+    return "coding";
+  }
+
+  @GetMapping("/gfa")
+  public String greenFoxAcademyMain(Model model) {
+    model.addAttribute("counter", studentService.count());
+    return "gfa";
+  }
+
+  @GetMapping("/gfa/list")
+  public String greenFoxAcademyStudentList(Model model) {
+    model.addAttribute("list", studentService.findAll());
+    return "list";
+  }
+
+  @GetMapping("/gfa/add")
+  public String addGreenFoxAcademyStudent() {
+
+    return "add";
+  }
+
+  @GetMapping("/gfa/save")
+  public String saveToGreenFoxAcademyStudentList(@RequestParam ("name") String name) {
+    studentService.save(name);
+    return "redirect:/gfa/list";
+  }
+
+  @GetMapping("/gfa/check")
+  public String studentCheckPage() {
+
+    return "check";
+  }
+
+  @GetMapping("/gfa/checking")
+  public String checkStudent(@RequestParam ("name") String name, Model model) {
+    model.addAttribute("isThere", studentService.check(name));
+    model.addAttribute("name", name);
+    return "checked";
+  }
+
+}

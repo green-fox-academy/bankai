eec2f1f4914116121af5ae69ad22892e249fef9d
Chika <ikechikaodili@gmail.com>
2018-12-13 09:19:50 +0100

    added templates and stylesheet

--- Week_Eight/Day_Three/src/main/java/com/example/demo/MainController.java
+++ Week_Eight/Day_Three/src/main/java/com/example/demo/MainController.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class MainController {
 
   private List<BankAccount> accounts;

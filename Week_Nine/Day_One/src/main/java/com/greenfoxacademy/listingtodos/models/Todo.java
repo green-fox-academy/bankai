3cce1b0ec1553c406d6110975e08b4baa9a27a6b
Chika <ikechikaodili@gmail.com>
2018-12-17 14:13:37 +0100

    completed listingtodos

---
+++ Week_Nine/Day_One/src/main/java/com/greenfoxacademy/listingtodos/models/Todo.java
@@ -0,0 +1,61 @@
+package com.greenfoxacademy.listingtodos.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public Todo() {
+  }
+
+  public Todo(String title) {
+    this.title = title;
+    this.urgent = false;
+    this.done = false;
+  }
+
+  public Todo(Long id, String title, boolean urgent, boolean done) {
+    this.id = id;
+    this.title = title;
+    this.urgent = urgent;
+    this.done = done;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+}
